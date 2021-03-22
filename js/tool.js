dfaa884a5dd2a62c374958d05e12cec4f1da4e03
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-03-22 15:08:48 +0100

    workstation

---
+++ js/tool.js
@@ -0,0 +1,26 @@
+// console.log('works');
+
+/*
+
+Mail problem
+
+1 chiedi all'utente la sua mail
+
+2 controlla che la mail sia tra quelle in elenco
+
+3 messaggio sull'esito del controllo 
+
+*/
+
+
+/*
+
+Dice problem
+
+1 generare numero random da 1 a 6 (per pc e per giocatore)
+
+2 chi fa punteggio più alto vince
+
+3 stampare il risultato
+
+*/
